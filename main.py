1442f161b124eeafc1e74d7e02aa5f4f91551437
KAMIKADZ4 <you@example.com>
2019-02-25 07:37:52 +0300

    Global remake

--- main.py
+++ main.py
@@ -1,95 +1,89 @@
 import pygame
 
 
-RESPAWN_PERIOD = 700
+RESPAWN_PERIOD, SPEED = 1000, 10
+run, move_tank = True, False
+time_of_last_shooting, fires = 0, []
+
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 900))
 screen.blit(pygame.image.load("img/background.png").convert(), [0, 0])
-tanks, run, move_tank, SPEED = [], True, False, 10
-time_of_last_shooting = 0
-no_coord, fires = [], []
-clock = pygame.time.Clock()
-clock.tick()
+
 
 pygame.mixer.init()
-background_sound = pygame.mixer.Sound('sound/background.wav')
-background_sound.set_volume(0.5)
+background_sound = pygame.mixer.Sound('sound/background_music.wav')
+background_sound.set_volume(0.1)
 background_sound.play(-1)
 
+
 shoot_clock = pygame.time.Clock()
-
-for x in list(range(0, 230)) + list(range(710, 901)):
-    n = []
-    for y in range(221):
-        n.append((x, y))
-    for y in range(710, 901):
-        n.append((x, y))
-    no_coord += n[:]
-
+clock = pygame.time.Clock()
+clock.tick()
 pygame.display.flip()
 
 
 class Player:
-    def __init__(self, pos):
-        self.x, self.y = pos
-        self.alive = True
-        self.time_of_last_death = 0
-        self.go = 'up'
-        self.time_of_last_shooting = 0
-        self.player_image = pygame.image.load('img/tank_up.png').convert_alpha()
+    def __init__(self, pos, img_n, go):
+        self.img_n, self.resp_pos, self.x, self.y, self.go = img_n, pos, pos[0], pos[1], go
+        self.time_of_last_death, self.time_of_last_shooting, self.alive = 0, 0, True
+        self.player_image = pygame.image.load(self.img_n + self.go + '.png').convert_alpha()
+
         screen.blit(self.player_image, self.player_image.get_rect(center=(self.x, self.y)))
         self.paint()
 
-    def move(self, x, y):
-        if self.alive:
-            if 870 >= self.x + x >= 20 and 870 >= self.y + y >= 20:
-                    self.x += x
-                    self.y += y
-                    self.paint()
+    def move(self, nx, ny):
+        if 880 >= self.x + nx >= 20 and 880 >= self.y + ny >= 20 and self.alive:
+            self.x += nx
+            self.y += ny
+            self.paint()
 
     def paint(self):
         screen.blit(self.player_image, self.player_image.get_rect(center=(self.x, self.y)))
 
     def img(self, name):
         if self.alive:
-            if name != 'img/fire.png':
-                self.go = name.split('_')[-1][0:-4]
-            self.player_image = pygame.image.load(name).convert_alpha()
-        if 'bang' in name:
-            self.player_image = pygame.image.load(name).convert_alpha()
+            self.go = name.split('_')[-1][0:-4]
+        self.player_image = pygame.image.load(name).convert_alpha()
 
     def fire(self):
         if self.alive:
-            BULLET_SPEED = 20
-            SHOOT_PERIOD = 500
+            BULLET_SPEED = 15
+            SHOOT_PERIOD = 600
+            z = pygame.mixer.Sound('sound/shoot.wav')
+            z.set_volume(0.1)
+            z.play()
             if not pygame.time.get_ticks() - self.time_of_last_shooting >= SHOOT_PERIOD:
                 return
             if self.go == 'up':
-                x, y = 0, -BULLET_SPEED
+                nx, ny = 0, -BULLET_SPEED
             elif self.go == 'down':
-                x, y = 0, BULLET_SPEED
+                nx, ny = 0, BULLET_SPEED
             elif self.go == 'left':
-                x, y = -BULLET_SPEED, 0
+                nx, ny = -BULLET_SPEED, 0
             elif self.go == 'right':
-                x, y = BULLET_SPEED, 0
-            ball = Bullet(self.x, self.y, x, y, len(fires))
+                nx, ny = BULLET_SPEED, 0
+
+            ball = Bullet(self.x, self.y, nx, ny)
             fires.append(ball)
             self.time_of_last_shooting = pygame.time.get_ticks()
-            pygame.mixer.Sound('sound/shoot.wav').play()
+
 
     def die(self):
         if self.alive:
             self.alive = False
             self.time_of_last_death = pygame.time.get_ticks()
-            pygame.mixer.Sound('sound/hit.wav').play()
+            z = pygame.mixer.Sound('sound/hit.wav')
+            z.set_volume(0.1)
+            z.play()
             self.respawn()
 
     def respawn(self):
         time_from_death = pygame.time.get_ticks() - self.time_of_last_death
         if time_from_death >= RESPAWN_PERIOD:
             self.alive = True
-            player.img('img/tank_' + self.go + '.png')
+            self.x, self.y = self.resp_pos
+            self.img(self.img_n + self.go + '.png')
             return
         # set frame
         frame_num = time_from_death // (RESPAWN_PERIOD // 7)
@@ -99,17 +93,17 @@ class Player:
 
 
 class Bullet:
-    def __init__(self, x, y, nx, ny, ind):
-        OFFSET = -3.1  # aligns bullet
+    def __init__(self, x, y, nx, ny):
+        OFFSET = -3  # aligns bullet
         self.flew = False
-        self.x, self.y, self.nx, self.ny, self.ind = x + OFFSET, y + OFFSET, nx, ny, ind
-        self.player_image = pygame.image.load('img/bullet.png').convert_alpha()
+        self.x, self.y, self.nx, self.ny = x + OFFSET, y + OFFSET, nx, ny
+        self.fire_image = pygame.image.load('img/bullet.png').convert_alpha()
 
     def growth(self):
         self.x += self.nx
         self.y += self.ny
-        if 900 >= self.x >= 0 and 900 >= self.y >= 0 and (self.x, self.y) not in no_coord:
-            screen.blit(self.player_image, [self.x, self.y])
+        if 900 >= self.x >= 0 and 900 >= self.y >= 0:
+            screen.blit(self.fire_image, [self.x, self.y])
         else:
             self.clear()
 
@@ -117,20 +111,8 @@ class Bullet:
         fires.pop(0)
 
 
-class Enemy(Player):
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-        self.time_of_last_death = 0
-        self.nx, self.ny = 0, 5
-        self.go = 'down'
-        self.alive = True
-        self.time_of_last_shooting = 0
-        self.player_image = pygame.image.load('img/enemy_down.png').convert_alpha()
-        self.paint()
-
-
-player = Player((450, 450))
-enemy = Enemy(100, 100)
+player_f = Player((450, 250), 'img/first_', 'down')
+player_s = Player((450, 450), 'img/second_', 'up')
 pygame.time.set_timer(1, 500)
 while run:
     screen.blit(pygame.image.load("img/background.png").convert(), [0, 0])
@@ -141,10 +123,10 @@ while run:
         elif event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_SPACE:
-                enemy.fire()
+                player_f.fire()
 
             if event.key == pygame.K_QUOTE:
-                player.fire()
+                player_s.fire()
 
         elif event.type == 2:
             pygame.time.set_timer(1, 0)
@@ -154,15 +136,15 @@ while run:
         fire.growth()
 
         # check kills
-        size_x, size_y = enemy.player_image.get_size()
-        killed_enemy = enemy.x - size_x // 2 <= fire.x \
-                       <= enemy.x + size_x // 2 and \
-                       enemy.y - size_y // 2 <= fire.y <= enemy.y + size_y // 2
+        size_x, size_y = player_s.player_image.get_size()
+        killed_enemy = player_s.x - size_x // 2 <= fire.x \
+                       <= player_s.x + size_x // 2 and \
+                       player_s.y - size_y // 2 <= fire.y <= player_s.y + size_y // 2
 
-        size_x, size_y = player.player_image.get_size()
-        killed_player = player.x - size_x // 2 <= fire.x \
-                        <= player.x + size_x // 2 and \
-                        player.y - size_y // 2 <= fire.y <= player.y + size_y // 2
+        size_x, size_y = player_f.player_image.get_size()
+        killed_player = player_f.x - size_x // 2 <= fire.x \
+                        <= player_f.x + size_x // 2 and \
+                        player_f.y - size_y // 2 <= fire.y <= player_f.y + size_y // 2
 
         # check: did the fire flew from tank
         if not fire.flew and (killed_player or killed_enemy):
@@ -173,52 +155,54 @@ while run:
         if killed_enemy:
             index = fires.index(fire)
             fires.pop(index)
-            enemy.die()
+            player_s.die()
         if killed_player:
             index = fires.index(fire)
             fires.pop(index)
-            player.die()
+            player_f.die()
+
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player.img('img/tank_left.png')
-        player.move(-SPEED, 0)
+        player_s.img('img/second_left.png')
+        player_s.move(-SPEED, 0)
 
     elif keys[pygame.K_RIGHT]:
-        player.img('img/tank_right.png')
-        player.move(SPEED, 0)
+        player_s.img('img/second_right.png')
+        player_s.move(SPEED, 0)
 
     elif keys[pygame.K_UP]:
-        player.img('img/tank_up.png')
-        player.move(0, -SPEED)
+        player_s.img('img/second_up.png')
+        player_s.move(0, -SPEED)
 
     elif keys[pygame.K_DOWN]:
-        player.img('img/tank_down.png')
-        player.move(0, SPEED)
+        player_s.img('img/second_down.png')
+        player_s.move(0, SPEED)
 
     if keys[pygame.K_a]:
-        enemy.img('img/enemy_left.png')
-        enemy.move(-SPEED, 0)
+        player_f.img('img/first_left.png')
+        player_f.move(-SPEED, 0)
 
     elif keys[pygame.K_d]:
-        enemy.img('img/enemy_right.png')
-        enemy.move(SPEED, 0)
+        player_f.img('img/first_right.png')
+        player_f.move(SPEED, 0)
 
     elif keys[pygame.K_w]:
-        enemy.img('img/enemy_up.png')
-        enemy.move(0, -SPEED)
+        player_f.img('img/first_up.png')
+        player_f.move(0, -SPEED)
 
     elif keys[pygame.K_s]:
-        enemy.img('img/enemy_down.png')
-        enemy.move(0, SPEED)
+        player_f.img('img/first_down.png')
+        player_f.move(0, SPEED)
 
-    if not enemy.alive:
-        enemy.respawn()
-    if not player.alive:
-        player.respawn()
 
-    player.paint()
-    enemy.paint()
+    if not player_s.alive:
+        player_s.respawn()
+    if not player_f.alive:
+        player_f.respawn()
+
+    player_f.paint()
+    player_s.paint()
     pygame.display.flip()
 
 
